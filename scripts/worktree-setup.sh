4e509519cfd7489dc983a81db4f224407ab6dcc7
Matheus Carnelutt <carneloot@proton.me>
2026-01-28 00:31:17 -0300

    chore: removing osgrep commands from worktree setup

--- scripts/worktree-setup.sh
+++ scripts/worktree-setup.sh
@@ -1,17 +1,8 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-git_dir=$(git rev-parse --git-dir)
-original_repo=$(cd "$git_dir/../../.." && pwd)
-worktree_root=$(git rev-parse --show-toplevel)
-
-# Create symlink to shared directory
-ln -s "$original_repo/.osgrep" "$worktree_root/.osgrep"
-
 mise trust
 mise up
-
-osgrep index
 
 bun install
 
